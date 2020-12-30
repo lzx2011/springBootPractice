f2e804ef83163c8ab5af1f05441828c8dcca2e4d
gary.liu <lzx20110@163.com>
2020-12-30 19:10:54 +0800

    optimize

--- src/main/java/com/lzhenxing/myproject/algorithm/MyPractice.java
+++ src/main/java/com/lzhenxing/myproject/algorithm/MyPractice.java
@@ -27,6 +27,11 @@ public class MyPractice {
         System.out.println("after:" + list.size());
     }
 
+    public static void testJson(){
+        String config = "\"EXT_NOTICE_CONFIG\":\"{\"ACCEPT\":\"2088000234SC_ACCEPT000001|ONLINE\",\"FINISH_SUCC\":\"2088000234SC_SUCCESS000001|ONLINE\",\"REFUNDED\":\"2088001477OS13019|ONLINE\",\"WAIT_PAY\":\"2088001477OS99671|ONLINE\",\"REFUND_TIMEOUT\":\"2088001477OS96197|ONLINE\",\"FINISH_FAIL\":\"2088000234SC_FAIL000001|ONLINE\",\"WAI_REFUND\":\"2088001477OS18439|ONLINE\"}\"";
+        //JSONObject.parseObject(config);
+    }
+
     public static void main(String[] args) {
         //practice();
         System.out.println("smfd".substring(1));
--- src/main/java/com/lzhenxing/myproject/algorithm/interview/MaxSumArray1BigThanArray2.java
+++ src/main/java/com/lzhenxing/myproject/algorithm/interview/MaxSumArray1BigThanArray2.java
@@ -1,47 +1,79 @@
 package com.lzhenxing.myproject.algorithm.interview;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  *   ClassName: MaxSumArray1BigThanArray2 <br/>
  *   Function:  by Microsoft China<br/>
- *   两个数组A和B的长度相同，找出 A[i] > B[i] 的最多的A的一个排序结果，保证B的顺序不变
+ *   两个数组A和B的长度相同，找出 A[i] > B[i] 数量最多的A的一个排序结果，保证A对应B的顺序不变
  *   A=[2,5,9,1]
  *   B=[6,3,7,4]
  *
- *   比如这个结果是 A=[2,9,1,5]
+ *   因为A[i] > B[i]的数共有两个，比如A的一个结果可以是 A=[9,1,2,5]
  *
  *   分析：
- *   对B排序获得两个新数组
- *   B1=[3,4,6,7]
+ *   对B排序获得新数组：bSort=[3,4,6,7]，被使用过后就把值改为 -1 (或者其他标志性的值)
  *
- *   B2[1]=0 没被使用
+ *   bSort 相对于B数组的顺序索引数组：bSortIndex=[1,3,0,2]
  *
- *   贪心策略：对A遍历，2由于不大于任何B1中的数，根据贪心策略 2 放在 B1中 7 的位置最好；5 放在刚好比4大的位置上最好,已经被放了的位置就不要再放了
- *   注意：因为要得出A的结果时保证B的顺序不变，这里对B已经排序了，所以要记录B之前的位置 （这里可以用索引倒序或者对象来存储值和下标）
- *   结果是 A=[2,9,1,5]
+ *   贪心策略：
+ *   1. 对A遍历和 bSort 倒着对比，2由于不大于任何bSort中的数，根据贪心策略 2 放在 bSort 中 7 的位置最好，再根据 bSortIndex 数组就可以找到 2 应该放的位置，将 7的位置置为 -1；
+ *   2. 5 放在刚好比4大的位置上最好,把 bSort 的值置为-1
+ *
+ *   注意：因为要得出A的结果对应B的顺序不变，这里对B已经排序了，所以要记录B之前的位置 （这里可以用索引数组记录或者对象来存储值和下标）
+ *
+ *   结果是 A=[9,1,2,5]
  *
  * @author zhenxing.liu
  * @date 2020/12/23
  */
 public class MaxSumArray1BigThanArray2 {
 
-    public static List<Integer> findOneMaxCount(List a, List b){
-
-        List<Integer> result = new ArrayList<>();
-
+    public static int[] findOneMaxCount(int[] a, int[] b){
+        if(a.length == 0){
+            return null;
+        }
+        int[] bSort = Arrays.copyOf(b, b.length);
+        Arrays.sort(bSort);
+        System.out.println("bSort: "  + Arrays.toString(bSort));
+        int[] bSortIndex = new int[b.length];
+        for(int i = 0; i < bSort.length; i++){
+            for(int j = 0; j < b.length; j++){
+                if(bSort[i] == b[j]){
+                    bSortIndex[i] = j;
+                }
+            }
+        }
+        int[] result = new int[a.length];
+        System.out.println("bSortIndex: " + Arrays.toString(bSortIndex));
+        for(int i = 0 ; i < a.length; i++){
+            boolean flag = false;
+            for(int j = bSort.length -1; j >= 0; j--){
+                if(a[i] > bSort[j] && bSort[j] != -1){
+                    result[bSortIndex[j]] = a[i];
+                    bSort[j] = -1;
+                    flag = true;
+                    break;
+                }
+            }
+            //a数组都比b小时
+            if(!flag){
+                for(int k = bSort.length -1; k >= 0; k--){
+                    if(bSort[k] != -1){
+                        result[bSortIndex[k]] = a[i];
+                        bSort[k] = -1;
+                        break;
+                    }
+                }
+            }
+        }
 
         return result;
     }
 
-
-
-
     public static void main(String[] args) {
-        List<Integer> a = new ArrayList<>(Arrays.asList(2,5,9,1));
-        List<Integer> b = new ArrayList<>(Arrays.asList(6,3,7,4));
-        System.out.println(findOneMaxCount(a, b));
+        int[] a = {2,5,9,1};
+        int[] b = {6,3,7,4};
+        System.out.println("result: " + Arrays.toString(findOneMaxCount(a, b)));
     }
 }
